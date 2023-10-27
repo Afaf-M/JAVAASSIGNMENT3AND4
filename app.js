b5b88c6f240ff24b3790120e25f5069194c7aa72
Afaf-M <130484736+Afaf-M@users.noreply.github.com>
2023-10-27 11:35:19 -0700

    Update app.js

--- app.js
+++ app.js
@@ -397,14 +397,468 @@
 
 // undefined(the array is empty after sorting and setting its length to 0)
 
+// Practice exercise 4.1
+// 1. Create a variable with a Boolean value.
+// 2. Output the value of the variable to the console.
+// 3. Check whether the variable is true and if so, output a message to the console,
+// using the following syntax:
+// if(myVariable){
+// //action
+// }
+// Logic Statements
+// [ 74 ]
+// 4. Add another if statement with an ! in front of the variable to check whether
+// the condition is not true, and create a message that will be printed to the
+// console in that instance. You should have two if statements, one with an
+// ! and the other without. You could also use an if and an else statement
+// instead—experiment!
+// 5. Change the variable to the opposite to see how the result changes.
+
+// ANSWER
+
+// // 1. Create a variable with a Boolean value.
+// const myVariable = true;
+
+// // 2. Output the value of the variable to the console.
+// console.log("The value of myVariable is: " + myVariable);
+
+// // 3. Check whether the variable is true and output a message to the console.
+// if (myVariable) {
+//     console.log("myVariable is true!");
+// }
+
+// // 4. Add another if statement with an '!' in front of the variable to check whether it's not true, and create a message for that case.
+// if (myVariable) {
+//     console.log("myVariable is true!");
+// } else {
+//     console.log("myVariable is not true!");
+// }
+
+// // 5. Change the variable to the opposite to see how the result changes.
+// const myVariableOpposite = !myVariable;
+
+// if (myVariableOpposite) {
+//     console.log("myVariableOpposite is true!");
+// } else {
+//     console.log("myVariableOpposite is not true!");
+// }
+
+// Practice exercise 4.2
+// 1. Create a prompt to ask the user's age
+// 2. Convert the response from the prompt to a number
+// 3. Declare a message variable that you will use to hold the console message for
+// the user
+// 4. If the input age is equal to or greater than 21, set the message variable to
+// confirm entry to a venue and the ability to purchase alcohol
+// 5. If the input age is equal to or greater than 19, set the message variable to
+// confirm entry to the venue but deny the purchase of alcohol
+// 6. Provide a default else statement to set the message variable to deny entry if
+// none are true
+// 7. Output the response message variable to the console
+
+
+// ANSWER
+
+// // 1. Create a prompt to ask the user's age
+// const userAge = prompt("Please enter your age:");
+
+// // 2. Convert the response from the prompt to a number
+// const age = parseInt(userAge);
+
+// // 3. Declare a message variable that you will use to hold the console message for the user
+// let message;
+
+// // 4. If the input age is equal to or greater than 21, set the message variable to confirm entry to a venue and the ability to purchase alcohol
+// if (age >= 21) {
+//     message = "You can enter the venue and purchase alcohol.";
+// }
+// // 5. If the input age is equal to or greater than 19, set the message variable to confirm entry to the venue but deny the purchase of alcohol
+// else if (age >= 19) {
+//     message = "You can enter the venue, but you cannot purchase alcohol.";
+// }
+// // 6. Provide a default else statement to set the message variable to deny entry if none are true
+// else {
+//     message = "You are denied entry to the venue.";
+// }
+
+// // 7. Output the response message variable to the console
+// console.log(message);
 
 
 
+// Practice exercise 4.3
+// 1. Create a Boolean value for an ID variable
+// 2. Using a ternary operator, create a message variable that will check whether
+// their ID is valid and either allow a person into a venue or not
+// 3. Output the response to the console
 
 
+// ANSWER
+
+// 1. Create a Boolean value for an ID variable
+// const hasValidID = true; // Change this to true or false as needed
+
+// // 2. Using a ternary operator, create a message variable that will check whether their ID is valid and either allow a person into a venue or not
+// const message = hasValidID ? "You are allowed to enter the venue." : "You are not allowed to enter the venue.";
+
+// // 3. Output the response to the console
+// console.log(message);
+
+// Practice exercise 4.4
+// As discussed in Chapter 1, Getting Started with JavaScript, the JavaScript function Math.
+// random() will return a random number in the range of 0 to less than 1, including 0
+// but not 1. You can then scale it to the desired range by multiplying the result and
+// using Math.floor() to round it down to the nearest whole number; for example, to
+// generate a random number between 0 and 9:
+// // random number between 0 and 1
+// let randomNumber = Math.random();
+// // multiply by 10 to obtain a number between 0 and 10
+// randomNumber = randomNumber * 10;
+// // removes digits past decimal place to provide a whole number
+// RandomNumber = Math.floor(randomNumber);
+// Chapter 4
+// [ 81 ]
+// In this exercise, we'll create a Magic 8-Ball random answer generator:
+// 1. Start by setting a variable that gets a random value assigned to it. The value
+// is assigned by generating a random number 0-5, for 6 possible results. You
+// can increase this number as you add more results.
+// 2. Create a prompt that can get a string value input from a user that you can
+// repeat back in the final output.
+// 3. Create 6 responses using the switch statement, each assigned to a different
+// value from the random number generator.
+// 4. Create a variable to hold the end response, which should be a sentence
+// printed for the user. You can assign different string values for each case,
+// assigning new values depending on the results from the random value.
+// 5. Output the user's original question, plus the randomly selected case
+// response, to the console after the user enters their question.
+
+// ANSWER
+
+// // 1. Generate a random number between 0 and 5 (for 6 possible results)
+// const randomValue = Math.floor(Math.random() * 6);
+
+// // 2. Create a clear and friendly prompt to get a user's question
+// const userQuestion = prompt("Welcome to the Magic 8-Ball!\nAsk a yes or no question:");
+
+// // 3. Create 6 responses using the switch statement based on the random number
+// let responseNumber;
+
+// switch (randomValue) {
+//     case 0:
+//         responseNumber = 0; // It is certain.
+//         break;
+//     case 1:
+//         responseNumber = 1; // Reply hazy, try again.
+//         break;
+//     case 2:
+//         responseNumber = 2; // Don't count on it.
+//         break;
+//     case 3:
+//         responseNumber = 3; // Signs point to yes.
+//         break;
+//     case 4:
+//         responseNumber = 4; // Outlook not so good.
+//         break;
+//     case 5:
+//         responseNumber = 5; // Ask again later.
+//         break;
+//     default:
+//         responseNumber = 6; // I'm sorry, I can't predict that.
+// }
+
+// // 4. Create the final response using an array of responses
+// const responses = [
+//     "It is certain.",
+//     "Reply hazy, try again.",
+//     "Don't count on it.",
+//     "Signs point to yes.",
+//     "Outlook not so good.",
+//     "Ask again later.",
+//     "I'm sorry, I can't predict that."
+// ];
+
+// const finalResponse = `You asked: ${userQuestion}\nMagic 8-Ball says: ${responses[responseNumber]}`;
+
+// // 5. Output the user's question and the Magic 8-Ball response to the console
+// console.log(finalResponse);
 
 
+// Practice exercise 4.5
+// 1. Create a variable called prize and use a prompt to ask the user to set the
+// value by selecting a number between 0 and 10
+// 2. Convert the prompt response to a number data type
+// 3. Create a variable to use for the output message containing the value "My
+// Selection: "
+// 4. Using the switch statement (and creativity), provide a response back
+// regarding a prize that is awarded depending on what number is selected
+// 5. Use the switch break to add combined results for prizes
+// 6. Output the message back to the user by concatenating your prize variable
+// strings and the output message string
+// ANSWER
+
+// // 1. Create a variable called prize and use a prompt to ask the user to set the value by selecting a number between 0 and 10
+// let prize = prompt("Choose a number between 0 and 10 to win a prize:");
+
+// // 2. Convert the prompt response to a number data type
+// prize = parseInt(prize);
+
+// // 3. Create a variable to use for the output message containing the value "My Selection: "
+// let message = "My Selection: ";
+
+// // 4. Using the switch statement, provide a response back regarding a prize that is awarded depending on what number is selected
+// switch (prize) {
+//     case 0:
+//         message += "Congratulations! You've won a car!";
+//         break;
+//     case 1:
+//         message += "You've won a vacation to a tropical island!";
+//         break;
+//     case 2:
+//         message += "You've won a new smartphone!";
+//         break;
+//     case 3:
+//         message += "You've won a gift card to your favorite restaurant!";
+//         break;
+//     case 4:
+//         message += "You've won a year's supply of chocolate!";
+//         break;
+//     case 5:
+//         message += "You've won a shopping spree!";
+//         break;
+//     case 6:
+//     case 7:
+//         message += "You've won a weekend getaway!";
+//         break;
+//     case 8:
+//     case 9:
+//     case 10:
+//         message += "Sorry, you didn't win this time. Better luck next time!";
+//         break;
+//     default:
+//         message += "Invalid selection. Please choose a number between 0 and 10.";
+// }
+
+// // 6. Output the message back to the user by concatenating your prize variable strings and the output message string
+// console.log(message);
+
+// Chapter projects CH4
+// Evaluating a number game
+// Ask the user to enter a number and check whether it's greater than, equal to, or less
+// than a dynamic number value in your code. Output the result to the user.
+
+// ANSWER
+
+// // 1. Define the dynamic number value
+// const dynamicNumber = 42; // You can change this value as desired
+
+// // 2. Ask the user to enter a number
+// const userNumber = parseFloat(prompt("Please enter a number:"));
+
+// // 3. Check whether the user's number is greater than, equal to, or less than the dynamic number
+// if (isNaN(userNumber)) {
+//     console.log("Invalid input. Please enter a valid number.");
+// } else if (userNumber > dynamicNumber) {
+//     console.log(`Your number (${userNumber}) is greater than ${dynamicNumber}.`);
+// } else if (userNumber === dynamicNumber) {
+//     console.log(`Your number (${userNumber}) is equal to ${dynamicNumber}.`);
+// } else {
+//     console.log(`Your number (${userNumber}) is less than ${dynamicNumber}.`);
+// }
 
 
+// PROJECT 2, Friend checker game
+// Ask the user to enter a name, using the switch statement to return a confirmation
+// that the user is a friend if the name selected is known in the case statements. You
+// can add a default response that you don't know the person if it's an unknown name.
+// Output the result into the console
 
 
+// ANSWER
+// // Ask the user to enter a name
+// const userName = prompt("Enter a name to check if it's a friend:");
+
+// // Using a switch statement to check the name
+// switch (userName.toLowerCase()) {
+//     case "alice":
+//     case "bob":
+//     case "charlie":
+//         console.log(`Yes, ${userName} is a friend!`);
+//         break;
+//     default:
+//         console.log(`Sorry, we don't know ${userName}.`);
+// }
+
+// Rock Paper Scissors game
+// This is a game between a player and the computer, where both will make a random
+// selection of either Rock, Paper, or Scissors (alternatively, you could create a version
+// using real player input!). Rock will beat out Scissors, Paper will beat out Rock, and
+// Scissors will beat out Paper. You can use JavaScript to create your own version of
+// this game, applying the logic with an if statement. Since this project is a little more
+// difficult, here are some suggested steps:
+// 1. Create an array that contains the variables Rock, Paper, and Scissors.
+// 2. Set up a variable that generates a random number 0-2 for the player and then
+// do the same for the computer's selection. The number represents the index
+// values in the array of the 3 items.
+// 3. Create a variable to hold a response message to the user. This can show the
+// random results for the player and then also the result for the computer of the
+// matching item from the array.
+// 4. Create a condition to handle the player and computer selections. If both are
+// the same, this results in a tie.
+// 5. Use conditions to apply the game logic and return the correct results.
+// There are several ways to do this with the condition statements, but you
+// could check which player's index value is bigger and assign the victory
+// accordingly, with the exception of Rock beating Scissors.
+// 6. Add a new output message that shows the player selection versus the
+// computer selection and the result of the game.
+
+
+// ANSWER
+// // 1. Create an array that contains the variables Rock, Paper, and Scissors.
+// const choices = ["Rock", "Paper", "Scissors"];
+
+// // 2. Ask the user to choose Rock, Paper, or Scissors.
+// const userChoice = prompt("Choose Rock, Paper, or Scissors:").trim().toLowerCase();
+
+// // 3. Validate the user's choice and generate a random choice for the computer.
+// if (choices.includes(userChoice)) {
+//     const computerChoice = choices[Math.floor(Math.random() * 3)];
+
+//     // 4. Create a variable to hold a response message to the user.
+//     let resultMessage;
+
+//     // 5. Determine the winner based on the game logic.
+//     if (userChoice === computerChoice) {
+//         resultMessage = "It's a tie!";
+//     } else if (
+//         (userChoice === "Rock" && computerChoice === "Scissors") ||
+//         (userChoice === "Paper" && computerChoice === "Rock") ||
+//         (userChoice === "Scissors" && computerChoice === "Paper")
+//     ) {
+//         resultMessage = `You win! ${userChoice} beats ${computerChoice}.`;
+//     } else {
+//         resultMessage = `Computer wins! ${computerChoice} beats ${userChoice}.`;
+//     }
+
+//     // 6. Output the user's choice, computer's choice, and the result of the game.
+//     console.log(`You chose: ${userChoice}`);
+//     console.log(`Computer chose: ${computerChoice}`);
+//     console.log(resultMessage);
+// } else {
+//     console.log("Invalid choice. Please choose Rock, Paper, or Scissors.");
+// }
+
+
+// Let's go through each of the quiz questions and provide the expected output:
+
+// Q1.What will be outputted to the console in this instance?
+
+// javascript
+// Copy code
+// const q = '1';
+// switch (q) {
+//   case '1':
+//     answer = "one";
+//     break;
+//   case 1:
+//     answer = 1;
+//     break;
+//   case 2:
+//     answer = "this is the one";
+//     break;
+//   default:
+//     answer = "not working";
+// }
+// console.log(answer);
+// Expected output: "one"
+
+// Q2.What will be outputted to the console in this instance?
+
+// javascript
+// Copy code
+// const q = 1;
+// switch (q) {
+//   case '1':
+//     answer = "one";
+//   case 1:
+//     answer = 1;
+//   case 2:
+//     answer = "this is the one";
+//     break;
+//   default:
+//     answer = "not working";
+// }
+// console.log(answer);
+// Expected output: "this is the one"
+
+// Q3.What will be outputted to the console in this instance?
+
+// javascript
+// Copy code
+// let login = false;
+// let outputHolder = "";
+// let userOkay = login ? outputHolder = "logout" : outputHolder = "login";
+// console.log(userOkay);
+// Expected output: "login"
+
+// Q4. What will be outputted to the console in this instance?
+
+// javascript
+// Copy code
+// const userNames = ["Mike", "John", "Larry"];
+// const userInput = "John";
+// let htmlOutput = "";
+// if (userNames.indexOf(userInput) > -1) {
+//   htmlOutput = "Welcome, that is a user";
+// } else {
+//   htmlOutput = "Denied, not a user ";
+// }
+// console.log(htmlOutput + ": " + userInput);
+// Expected output: "Welcome, that is a user: John"
+
+// Q5.What will be outputted to the console in this instance?
+
+// javascript
+// Copy code
+// let myTime = 9;
+// let output;
+// if (myTime >= 8 && myTime < 12) {
+//   output = "Wake up, it's morning";
+// } else if (myTime >= 12 && myTime < 13) {
+//   output = "Go to lunch";
+// } else if (myTime >= 13 && myTime <= 16) {
+//   output = "Go to work";
+// } else if (myTime > 16 && myTime < 20) {
+//   output = "Dinner time";
+// } else if (myTime >= 22) {
+//   output = "Time to go to sleep";
+// } else {
+//   output = "You should be sleeping";
+// }
+// console.log(output);
+// Expected output: "Wake up, it's morning"
+
+// Q6.What will be outputted to the console in this instance?
+
+// javascript
+// Copy code
+// let a = 5;
+// let b = 10;
+// let c = 20;
+// let d = 30;
+// console.log(a > b || b > a);    // false
+// console.log(a > b && b > a);    // false
+// console.log(d > b || b > a);    // true
+// console.log(d > b && b > a);    // true
+// The comments indicate the expected output for each line.
+
+// Q7. What will be outputted to the console in this instance?
+
+// javascript
+// Copy code
+// let val = 100;
+// let message = (val > 100) ? `${val} was greater than 100` : `${val} was LESS or Equal to 100`;
+// console.log(message);    // "100 was LESS or Equal to 100"
+// let check = (val % 2) ? `Odd` : `Even`;
+// check = `${val} is ${check}`;
+// console.log(check);      // "100 is Even"
+// The comments indicate the expected output for each line
